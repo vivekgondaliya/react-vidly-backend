e18f956fb3cae0963eb9835b9be8997e884ad8cd
vivekgondaliya <vg13@zips.uakron.edu>
2018-12-02 22:12:24 -0800

    Enabled Save Movie feature

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,32 +1,31 @@
 import http from "./httpService";
 import { apiUrl } from "../config.json";
 
+const API_ENDPOINT = `${apiUrl}/movies`;
 //get Movies
 export function getMovies() {
-  return http.get(`${apiUrl}/movies`);
+  return http.get(`${API_ENDPOINT}`);
 }
 
 //get a Movie
 export function getMovie(movieId) {
-  return http.get(`${apiUrl}/movies/${movieId}`);
+  return http.get(`${API_ENDPOINT}/${movieId}`);
 }
 
 //delete a movie
 export function deleteMovie(movieId) {
-  return http.delete(`${apiUrl}/movies/${movieId}`);
+  return http.delete(`${API_ENDPOINT}/${movieId}`);
 }
 
 //save a movie
-export async function saveMovie(movie) {
-  // const { data: movies } = await getMovies();
-  // let movieInDb = movies.find(m => m._id === movie._id) || {};
-  // movieInDb.title = movie.title;
-  // movieInDb.genre = genresAPI.genres.find(g => g._id === movie.genreId);
-  // movieInDb.numberInStock = movie.numberInStock;
-  // movieInDb.dailyRentalRate = movie.dailyRentalRate;
-  // if (!movieInDb._id) {
-  //   movieInDb._id = Date.now().toString();
-  //   movies.push(movieInDb);
-  // }
-  // return movieInDb;
+export function saveMovie(movie) {
+  //update existing movie in DB
+  if (movie._id) {
+    const body = { ...movie };
+    delete body._id;
+    return http.put(`${API_ENDPOINT}/${movie._id}`, body);
+  }
+
+  //if it's a new movie
+  return http.post(API_ENDPOINT, movie);
 }
